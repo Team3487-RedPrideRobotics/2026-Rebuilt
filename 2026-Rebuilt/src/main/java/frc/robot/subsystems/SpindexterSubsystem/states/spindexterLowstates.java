fb78b830ada7d13333ff80b4a9f9477c587d0093
Antoine <pandabobandapaga@gmail.com>
2026-02-09 16:50:13 -0500

    spindexter slow to low
    2/9/2026
    
    (wrong name)
    
    -A

--- 2026-Rebuilt/src/main/java/frc/robot/subsystems/SpindexterSubsystem/states/spindexterLowstates.java
+++ 2026-Rebuilt/src/main/java/frc/robot/subsystems/SpindexterSubsystem/states/spindexterLowstates.java
@@ -3,11 +3,11 @@ package frc.robot.subsystems.SpindexterSubsystem.states;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.SpindexterSubsystem.SpindexterSubsytem;
 
-public class spindexterSlowstates extends Command{
+public class spindexterLowstates extends Command{
     
     SpindexterSubsytem subsystem;
 
-    public spindexterSlowstates( SpindexterSubsytem Subsystem){
+    public spindexterLowstates( SpindexterSubsytem Subsystem){
         
         subsystem = Subsystem;
         addRequirements(Subsystem);
