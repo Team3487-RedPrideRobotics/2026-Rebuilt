970a82623e86bd8332b93490b6740a49c1b99dab
Antoine <pandabobandapaga@gmail.com>
2026-02-19 16:28:40 -0500

    ClimberPivotoutstate works now
    2/19/26
    
    -A

--- 2026-Rebuilt/src/main/java/frc/robot/RobotContainer.java
+++ 2026-Rebuilt/src/main/java/frc/robot/RobotContainer.java
@@ -184,7 +184,7 @@ public class RobotContainer {
         m_drivetrain.registerTelemetry(m_logger::telemeterize);
 
     operatorController.povUp().whileTrue(new ClimberClimbUpstate(m_Climber));
-    //operatorController.povLeft().whileTrue(new ClimberPivotOutstate(m_Climber));
+    operatorController.povLeft().whileTrue(new ClimberPivotOutstate(m_Climber));
     operatorController.povRight().whileTrue(new ClimberPivotInstate(m_Climber)); 
     operatorController.povDown().whileTrue(new ClimberClimbDownstate(m_Climber));
     }  
@@ -199,7 +199,7 @@ public class RobotContainer {
                                                                                   ).withTimeout(5));
         //Climber:
         NamedCommands.registerCommand("Climber Arm Extend", new ClimberClimbUpstate(m_Climber).withTimeout(2));
-       // NamedCommands.registerCommand("Climber Arm Out", new ClimberPivotOutstate(m_Climber).withTimeout(1));
+        NamedCommands.registerCommand("Climber Arm Out", new ClimberPivotOutstate(m_Climber).withTimeout(1));
         NamedCommands.registerCommand("Climber Arm In", new ClimberPivotInstate(m_Climber).withTimeout(1));
         NamedCommands.registerCommand("Climber Hold Arm Up", new ClimberHoldUpstate(m_Climber).withTimeout(1));
         NamedCommands.registerCommand("Climber Hold Arm Down", new ClimberHoldDownstate(m_Climber).withTimeout(1));
