0dc25611109da2f4e1a0dbf13798276fd5203d93
3487 Red Pride Robotics <team3487@gmail.com>
2026-02-23 16:34:29 -0500

    robot container pivot out changes
    2/23/26
    
    -A

--- 2026-Rebuilt/src/main/java/frc/robot/RobotContainer.java
+++ 2026-Rebuilt/src/main/java/frc/robot/RobotContainer.java
@@ -35,7 +35,7 @@ import frc.robot.subsystems.ClimberSubsystem.states.ClimberGetDirectionstate;
 import frc.robot.subsystems.ClimberSubsystem.states.ClimberHoldDownstate;
 import frc.robot.subsystems.ClimberSubsystem.states.ClimberHoldUpstate;
 import frc.robot.subsystems.ClimberSubsystem.states.ClimberPivotInstate;
-import frc.robot.subsystems.ClimberSubsystem.states.ClimberPivotOutstate;
+import frc.robot.subsystems.ClimberSubsystem.states.ClimberPiviotOutstate;
 import frc.robot.subsystems.IntakeSubsystem.IntakeSubsystem;
 import frc.robot.subsystems.IntakeSubsystem.states.IntakePivotDownState;
 import frc.robot.subsystems.IntakeSubsystem.states.IntakePivotUpState;
@@ -204,7 +204,7 @@ public class RobotContainer {
         operatorController.leftBumper().whileTrue(new ClimberHoldDownstate(m_Climber));
 
     operatorController.povUp().whileTrue(new ClimberClimbUpstate(m_Climber));
-    operatorController.povLeft().whileTrue(new ClimberPivotOutstate(m_Climber));
+    operatorController.povLeft().whileTrue(new ClimberPiviotOutstate(m_Climber));
     operatorController.povRight().whileTrue(new ClimberPivotInstate(m_Climber)); 
     operatorController.povDown().whileTrue(new ClimberClimbDownstate(m_Climber));
     }  
@@ -224,7 +224,7 @@ public class RobotContainer {
                                                                                   ).withTimeout(10));
         //Climber:
         NamedCommands.registerCommand("Climber Arm Extend", new ClimberClimbUpstate(m_Climber).withTimeout(2));
-        NamedCommands.registerCommand("Climber Arm Out", new ClimberPivotOutstate(m_Climber).withTimeout(1));
+        NamedCommands.registerCommand("Climber Arm Out", new ClimberPiviotOutstate(m_Climber).withTimeout(1));
         NamedCommands.registerCommand("Climber Arm In", new ClimberPivotInstate(m_Climber).withTimeout(1));
         NamedCommands.registerCommand("Climber Hold Arm Up", new ClimberHoldUpstate(m_Climber).withTimeout(1));
         NamedCommands.registerCommand("Climber Hold Arm Down", new ClimberHoldDownstate(m_Climber).withTimeout(1));
