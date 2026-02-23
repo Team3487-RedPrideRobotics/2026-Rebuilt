6b848a65e2b52a7c9a585adf104fd99de9e8fd99
3487 Red Pride Robotics <team3487@gmail.com>
2026-02-23 16:31:25 -0500

    pivot out file name change
    2/23/26
    
    -A

--- 2026-Rebuilt/src/main/java/frc/robot/subsystems/ClimberSubsystem/states/ClimberPiviotOutstate.java
+++ 2026-Rebuilt/src/main/java/frc/robot/subsystems/ClimberSubsystem/states/ClimberPiviotOutstate.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems.ClimberSubsystem.states;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ClimberSubsystem.ClimberSubsystem;
 
-public class ClimberPivotOutstate extends Command{
+public class ClimberPiviotOutstate extends Command{
      
     ClimberSubsystem subsystem;
 
@@ -11,7 +11,7 @@ public class ClimberPivotOutstate extends Command{
 
     boolean done;
 
-    public ClimberPivotOutstate(ClimberSubsystem Subsystem){
+    public ClimberPiviotOutstate(ClimberSubsystem Subsystem){
         
         super();
 
@@ -48,3 +48,4 @@ public class ClimberPivotOutstate extends Command{
     }
 
 }
+// name change
